2dbfd8bc9a7930085b3bd12d9abdd9c6fadf35b5
Isaac Silva <isaacsilta.info@gmail.com>
2026-09-24 18:44:13 -0300

    first commit

---
+++ src/main/java/br/com/isaacsilva/tech/fastfood/gateway/FastfoodGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.isaacsilva.tech.fastfood.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodGatewayApplication.class, args);
+	}
+
+}
